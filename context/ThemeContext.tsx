efeb72696f6de46914326fac6bda74fec20e2a49
Tey <teyextreme@outlook.com>
2024-11-06 17:43:50 -0300

    theme provider

---
+++ context/ThemeContext.tsx
@@ -0,0 +1,29 @@
+import React, { createContext, useState, useContext } from "react";
+import { themes } from "@/theme";
+
+interface ThemeContextType {
+    theme : any
+    toggleTheme?: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | null>(null);
+
+export const ThemeProvider: React.FC<React.PropsWithChildren<unknown>> = ({ children }) => {
+    const [theme, setTheme] = useState(themes.light);
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === themes.light ? themes.dark : themes.light));
+      };
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+          {children}
+        </ThemeContext.Provider>
+      );
+}
+
+export const useTheme = (): ThemeContextType => {
+    const context = useContext(ThemeContext);
+    if (!context) {
+      throw new Error("UseRefresh ser Utilizado dentro de um RefreshProvider");
+    }
+    return context;
+  };
---
+++ theme/index.ts
@@ -0,0 +1,64 @@
+export const themes = {
+    light:{
+        colors: {
+            primary: "#000000",
+            gray: "#636363",
+            lightGray: '#d6d6d6',
+            neutral: '#e5e5e5',
+            blue: "#1FBCFF",
+            lightBlue: '#DFEFFF',
+            green: "#00DAB7",
+            yellow: "#FFC412",
+        },
+        fontStyle:{
+            interBlack: "Inter-Black",
+            interBold: "Inter-Bold",
+            interExtraBold: "Inter-ExtraBold",
+            interExtralight: "Inter-ExtraLight",
+            interLight: "Inter-Light",
+            interMedium: "Inter-Medium",
+            interRegular: "Inter-Regular",
+            interSemibold: "Inter-SemiBold",
+            interThin: "Inter-Thin",
+        },
+        fontSize:{
+            xs:12,
+            sm:14,
+            base:16,
+            lg:18,
+            xl:20,
+        },
+    },
+    
+    dark:{
+        colors: {
+            primary: "#000000",
+            gray: "#636363",
+            lightGray: '#d6d6d6',
+            neutral: '#e5e5e5',
+            blue: "#1FBCFF",
+            lightBlue: '#DFEFFF',
+            green: "#00DAB7",
+            yellow: "#FFC412",
+        },
+        fontStyle:{
+            interBlack: "Inter-Black",
+            interBold: "Inter-Bold",
+            interExtraBold: "Inter-ExtraBold",
+            interExtralight: "Inter-ExtraLight",
+            interLight: "Inter-Light",
+            interMedium: "Inter-Medium",
+            interRegular: "Inter-Regular",
+            interSemibold: "Inter-SemiBold",
+            interThin: "Inter-Thin",
+        },
+        fontSize:{
+            xs:12,
+            sm:14,
+            base:16,
+            lg:18,
+            xl:20,
+        },
+    }
+
+}
